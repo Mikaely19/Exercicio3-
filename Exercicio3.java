8a102027974fb39d73e90aecce53a6571c3870e7
Mikaely19 <125111409306@ulife.com.br>
2025-05-05 23:29:14 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -4,21 +4,17 @@ public class App {
         int[] vetor = new int[20];
         int contadorPares = 0;
 
-        // Leitura dos números
         System.out.println("Digite 20 números inteiros:");
-        for (int i = 0; i < vetor.length; i++) {
+        for (int i = 0; i < vetor; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             vetor[i] = scanner.nextInt();
         }
 
-        // Contagem de números pares
-        for (int num : vetor) {
+        for (int : vetor) {
             if (num % 2 == 0) {
                 contadorPares++;
             }
         }
-
-        // Exibição do resultado
         System.out.println("Quantidade de números pares: " + contadorPares);
         
         scanner.close();
